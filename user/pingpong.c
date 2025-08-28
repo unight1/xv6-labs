4d25dcb52b8aece4fcd06e188a201fda08c663c0
unight1 <2350217@tongji.edu.cn>
2025-08-28 16:04:07 +0800

    Update pingpong.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,17 +4,12 @@
 int main(int argc, char *argv[]) {
     int parent_to_child[2];  
     int child_to_parent[2];
-    
     pipe(parent_to_child);
     pipe(child_to_parent);
-    
     char buf[1];  
-    
     if(fork() == 0) {  
         close(parent_to_child[1]);  
         close(child_to_parent[0]);  
-        
-        
         read(parent_to_child[0], buf, 1);
         printf("%d: received ping\n", getpid());
         
